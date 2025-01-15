ea08e818ca8ac5351df64148a40f15aa1dbb49e9
bencarnes <35011668+bencarnes@users.noreply.github.com>
2025-01-15 03:10:41 +0000

    progress towards word lists

---
+++ typinggame/wordlists.js
@@ -0,0 +1,73 @@
+
+const WordLists = [
+    {
+        name: "Left Upper",
+        words: [
+            "sad",
+            "red",
+            "fed",
+            "was",
+            "dew",
+            "dad",
+            "saw",
+            "we",
+            "as",
+            "rest",
+            "far",
+            "trade",
+            "waste",
+            "see",
+            "sew",
+            "few",
+            "wet",
+            "eat",
+            "fast",
+            "set",
+            "tea",
+            "west",
+            "safe",
+            "feed",
+            "taste",
+            "seed",
+            "date",
+            "dare",
+            "deed",
+            "deer",
+            "fade",
+            "fear",
+            "free",
+            "raft",
+            "rate",
+            "read",
+            "reed",
+            "ward",
+            "wear",
+            "weed",
+            "ease",
+            "east",
+            "afar",
+            "area",
+            "awed",
+            "awe",
+            "are",
+            "art",
+            "ate",
+            "ear",
+            "eat",
+            "era",
+            "ewe"
+        ]
+    },
+    {
+        name: "Home Row",
+        words: [
+            "ask",
+            "dad",
+            "sad",
+            "fad",
+            "add"
+        ]
+    }
+]
+
+export default WordLists;
